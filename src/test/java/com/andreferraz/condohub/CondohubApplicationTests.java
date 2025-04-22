11e91d963fcad6c2efe19abc12b1b646f0cf9754
André Ferraz <oandreferraz@gmail.com>
2025-04-22 19:59:16 -0300

    First commit

---
+++ src/test/java/com/andreferraz/condohub/CondohubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andreferraz.condohub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CondohubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
